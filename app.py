7a30b018cf22e7ac5909f4f72459ef9ab0a81018
imjayyy <mujtabajafri3@gmail.com>
2020-05-01 22:26:08 +0500

    'allnew'

---
+++ app.py
@@ -0,0 +1,304 @@
+from datetime import datetime,timedelta 
+
+from flask import (Flask, flash, jsonify, redirect, render_template, request,
+                   session, url_for)
+
+from dbcon import (
+    add_purchase, add_sales, addinform, addQuantity, get_comp, get_data,
+    get_sales, get_variety, getTodaysPurchaserRecords, getTodaysSaleRecords,
+    sform, addnewform, update_inventory, add_item, getOutOfStock, searchform,
+    get_purchase, get_productname, db, getQuantityFromWh, getSalesOf6Months)
+
+app = Flask(__name__)
+app.secret_key = 'loginner'
+
+users = {'admin' : 'pass!@#$admin',
+        'vimal'   : 'hemnani!@#',}
+
+# @app.route('/')
+# def main():
+#     return render_template(url_for('loginform'))
+
+@app.route("/account" , methods=['Post','GET'])
+def account():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    form = searchform()
+    form.transaction.choices = [('Payable','Payable'), ('Recievable','Recievable')]
+    form.transaction.label = "Select Accounts"
+
+    column = ['Name Name',	
+    "Product Name",	
+    "Name's City",
+    "Quantity",	
+    "Variety",	
+    "Company name",	
+    "Total Price",	
+    "Date",	
+    "Transaction"]
+
+    if request.method == 'POST':
+        if form.transaction.data == "Recievable":
+            _sale = {"Name":form.name.data,
+                    "Productname":form.product.data,
+                    "City": form.city.data,
+                    "Brand":form.Brand.data,
+                    "Variety": form.variety.data,
+                    "Company": form.c_comp.data,
+                    "transaction" : form.transaction.data}
+            querry = {"transaction" : "Credit"}
+            for i in (_sale):
+                if len(_sale[i])>0:
+                    querry[i] = _sale[i]
+
+            return render_template('account.html', arr=db.sales.find({"transaction" : "Credit"}), form=form, 
+            heading = 'Accounts '+   (form.transaction.data), redirect = redirect)
+        if form.transaction.data == "Payable":
+            _sale = {"Name":form.name.data,
+                    "Productname":form.product.data,
+                    "City": form.city.data,
+                    "Brand":form.Brand.data,
+                    "Variety": form.variety.data,
+                    "Company": form.c_comp.data,
+                    "transaction" : form.transaction.data}
+            querry = {"transaction" : "Credit"}
+            for i in (_sale):
+                if len(_sale[i])>0:
+                    querry[i] = _sale[i]
+
+            return render_template('account.html', arr=db.purchase.find({"transaction" : "Credit"}), 
+            form=form, heading = 'Accounts '+(form.transaction.data), redirect = redirect)
+
+    return render_template('account.html', arr=db.sales.find({"transaction" : "Credit"}),
+     form=form, heading = 'Accounts '+'Recievable', redirect = redirect)
+
+@app.route('/login') 
+def loginform():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username and password and username in users and users[username] == password:
+        session['logged_in'] = True
+        return redirect(url_for('home'))
+
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
+
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    values = getQuantityFromWh()
+    labels = ['Warehouse 1', 'Warehouse 2', 'Warehouse 3']
+    colors = ["#F7464A", "#46BFBD", "#FDB45C"]
+    
+    (bar_labels, bar_values) = getSalesOf6Months()
+    max2 = max(bar_values)
+    outofstock = getOutOfStock(100)
+
+    return render_template('home.html', max=17000, 
+    set = zip(values, labels, colors ), 
+    max2 = max2, labels=bar_labels, values=bar_values,
+    outofstock=outofstock)
+
+@app.route("/inventory")
+def inventory():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    arr = get_data()
+    return render_template('inventory.html', arr=arr, redirect = redirect)
+
+@app.route("/sales"  , methods=['Post','GET'])
+def sales():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    form = searchform()
+
+    if request.method == 'POST':
+        _sale = {"Name":form.name.data,
+                "Productname":form.product.data,
+                "City": form.city.data,
+                "Brand":form.Brand.data,
+                "Variety": form.variety.data,
+                "Company": form.c_comp.data,
+                "transaction" : form.transaction.data}
+        querry = {}
+        for i in (_sale):
+            if len(_sale[i])>0:
+                querry[i] = _sale[i]
+
+        return render_template('sales.html', arr=get_sales(querry), form=form, redirect = redirect)
+    return render_template('sales.html', arr=get_sales(), form=form, redirect = redirect)
+
+@app.route("/addinventory" , methods=['Post','GET'])
+def addinventory():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    form = addinform()
+    form.product.choices = get_productname()
+    form_addnew =  addnewform()   
+    # if form.submit.data and form.validate_on_submit:
+    if request.form.get('Submit') == 'Submit': 
+        purchase = {
+                "Productname":form.product.data,
+                "Variety": form.variety.data,
+                "Brand":form.Brand.data,
+                "Name": form.name.data,
+                "City": form.city.data,
+                "Company": form.c_comp.data,
+                "Quantity": form.quan.data,
+                "Total": form.price.data,
+                "transaction" : form.transaction.data,
+                "Date": datetime.now() 
+                }
+       
+
+
+        if addQuantity(form.product.data,form.variety.data,form.Brand.data, form.quan.data, form.warehouse.data):
+            add_purchase(purchase)
+            flash("Purchase Record Created.!!!", 'Success') 
+            # form = addinform()
+            return redirect(url_for('addinventory'))
+        else:
+            flash("Could not find product in Inventory.!!!", 'ERROR')
+
+    if request.form.get('Submit') == 'Add Item':
+        purchase = {"Productname":form_addnew.product.data,
+                   "Variety": form_addnew.variety.data,
+                   "Brand":form_addnew.Brand.data,
+                    "Name": form_addnew.name.data,
+                    "City": form_addnew.city.data,
+                    "Company": form_addnew.c_comp.data,
+                    "Quantity": form_addnew.quan.data,
+                    "Total": form_addnew.price.data,
+                    "transaction" : form_addnew.transaction.data,
+                    "Date": datetime.now() }
+
+        dicts = {"Productname" : form_addnew.product.data, 
+                 "Variety"     : form_addnew.variety.data, 
+                 "Brand"       : form_addnew.Brand.data, 
+                 "Quantity"    : form_addnew.quan.data,
+                 "Purchase_price": form_addnew.price.data,
+                 "Warehouse" : form_addnew.warehouse.data,
+                 "Date" : datetime.now()
+                 }
+
+
+
+        if add_item(dicts) and add_purchase(purchase):
+            flash("Purchase Record Created and added item in inventory.!!!", 'Success')
+            return redirect(url_for('addinventory'))
+        else:
+            flash("There was an Error.!!!", 'ERROR')
+
+    return render_template('addinventory.html', form = form, form_addnew = form_addnew)
+
+@app.route("/salesform" , methods=['Post','GET'])
+def salesform():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    form = sform()
+    form.product.choices = get_productname()    
+    if request.method == 'POST':
+        _sale = {"Name":form.name.data,
+                "Productname":form.product.data,
+                "City": form.city.data,
+                "Quantity": form.quan.data,
+                "Brand":form.Brand.data,
+                "Variety": form.variety.data,
+                "Company": form.c_comp.data,
+                "Total" : form.price.data,
+                "Warehouse" : form.warehouse.data,
+                "transaction" : form.transaction.data,
+                "Date": datetime.now() }
+        
+        
+        if update_inventory(form.product.data,form.variety.data,form.Brand.data, form.quan.data, form.warehouse.data):
+            add_sales(_sale)
+            flash("Sales Record Created.!!!", 'Success')
+            return redirect(url_for('salesform'))
+        else:
+            flash("Could not find product in Inventory Check Warehouse Number Or Quantity.!!!", 'ERROR')
+
+    return render_template('salesform.html', form = form)
+
+@app.route('/purchases', methods = ['POST', 'GET'])
+def purchases():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    form = searchform()
+    form.c_comp.label = 'Company'
+    form.name.label = "Name"
+
+    if request.method == 'POST':
+        _sale = {"Name":form.name.data,
+                "Productname":form.product.data,
+                "City": form.city.data,
+                "Brand":form.Brand.data,
+                "Variety": form.variety.data,
+                "Company": form.c_comp.data,
+                "transaction" : form.transaction.data}
+        querry = {}
+        for i in (_sale):
+            if len(_sale[i])>0:
+                querry[i] = _sale[i]
+
+        return render_template('purchases.html', arr=get_purchase(querry), form=form, redirect = redirect)
+    return render_template('purchases.html', arr=get_purchase(), form=form, redirect = redirect)
+    
+@app.route("/variety/<product>")
+def product(product):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    arr = [("", "Choose")]
+    for i in get_variety(product):
+        obj = {}
+        obj["value"] =  i 
+        obj["name"]  =  i 
+        arr.append(obj)
+    return jsonify({'variety': arr})
+
+@app.route("/Brand/<product>/<variety>")
+def Brand(product, variety):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    arr = [('choose', 'Choose')]
+    for i in get_comp(product, variety):
+        obj = {}   
+        obj["value"] =  i 
+        obj["name"]  =  i 
+        arr.append(obj)
+    return jsonify({'Brand': arr})
+
+@app.route("/reports")
+def reports():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    querry = {'$lte': datetime.today(), '$gte': datetime.today()-timedelta(1)}
+    purchase_records, purchase_amount = (getTodaysPurchaserRecords(querry))
+    purchase_amount = 'Rs. ' + str((format (purchase_amount, ',d'))) + '/-'
+    sale_records, sale_amount = getTodaysSaleRecords(querry)
+    
+    sale_amount = 'Rs. ' + str((format (sale_amount, ',d'))) + '/-'
+
+    return render_template('reports.html', purchase_amount=purchase_amount, 
+                                        purchase_records=purchase_records, 
+                                        sale_amount=sale_amount, 
+                                        sale_records = sale_records,
+                                        format = format)
+
+@app.route('/logout')
+def logout():
+    if not session.get('logged_in'):
+        return "Not logged in"
+    else:
+        session['logged_in'] = False
+    return render_template('logout.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ dbcon.py
@@ -0,0 +1,236 @@
+from datetime import date, datetime,timedelta
+
+import pymongo
+from flask_wtf import FlaskForm
+from wtforms import (IntegerField, RadioField, SelectField, SubmitField,
+                     TextAreaField, TextField, ValidationError, validators)
+
+
+
+
+today = date.today().strftime("%d/%m/%Y")
+sample = {"Productname":"Productname",
+"Quantity": 123,
+"Variety": "variety",
+"Brand": "Brand",
+"Purchase_Price" : 2.1,
+"Date": today
+}
+
+myclient = pymongo.MongoClient("mongodb+srv://mujtaba:developer@hsc-vp2me.mongodb.net/test?retryWrites=true&w=majority")
+
+
+mydb = myclient["HSC"]
+inventory = mydb["inventory"]
+sales = mydb["sales_records"]
+purchase = mydb["purchase_records"]
+
+class db():
+    inventory = mydb["inventory"]
+    sales = mydb["sales_records"]
+    purchase = mydb["purchase_records"]
+
+
+
+def update_inventory(product, variety, brand, quantity, warehouse): #Subtracting Quantity in existing item (When there is a Sale) 
+    arr = inventory.find({"Productname":product,"Variety": variety,"Brand":brand, "Warehouse": int(warehouse)} )
+    new_quantity = arr[0]['Quantity'] - quantity
+    if new_quantity < 0:
+        return False
+    result = inventory.update_one({"Productname":product,"Variety": variety,"Brand":brand} ,{ '$set' : {'Quantity': (new_quantity) } })
+    return True
+
+def add_sales(dicts):#Add a document in sale Records
+    sales.insert(dicts)
+
+def add_item(dicts):#Add a document in Inventory
+    try:
+        inventory.insert(dicts)
+        return True
+    except:
+        return False
+
+def add_purchase(dicts):#Add a document in purchase record and returns Boolean
+    try:
+        purchase.insert(dicts)
+        return True
+    except:
+        return False
+
+def get_data():#Returns all the inventory Records from Database
+    return inventory.find({})
+
+def get_sales(querry = {}): #Returns all the sale Records from Database
+    return sales.find(querry).sort([('Date',pymongo.DESCENDING)])
+
+def get_purchase(querry = {}): #Returns all the sale Records from Database
+    return purchase.find(querry).sort([('Date',pymongo.DESCENDING)])
+
+def get_variety(prod):#Shortlisting varieties from crops, used for dropdown menus
+    arr = inventory.distinct('Variety',{"Productname": prod})
+    return arr
+
+def get_comp(prod, var): #Shortlisting Brand names from crops and varieties, used for dropdown menus
+    arr =inventory.distinct("Brand",{"Productname": prod, "Variety":var})
+    return arr
+
+def get_productname():#Getting all the product names in the inventory
+    arr = [('',"Choose")]
+    for i in inventory.distinct('Productname'):
+        tup = ( i , i )
+        arr.append(tup)
+    return arr
+
+def addQuantity(prd, var, comp, q , w): #adding Quantity in existing item 
+    try:
+        arr = inventory.find({"Productname":prd,"Variety": var,"Brand":comp, "Warehouse": w} )
+        new_quantity = arr[0]['Quantity'] + q
+        if new_quantity < 0:
+            return (arr[0]['Quantity'])
+        result = inventory.update_one({"Productname":prd,"Variety": var,"Brand":comp} ,{ '$set' : {'Quantity': (new_quantity) } })
+        return result.matched_count > 0 
+    except:
+        return False
+
+def getTodaysPurchaserRecords(querry): #Returns total amount of purchase and total records of purchase of present day
+    arr = []
+    amount = 0
+    for i in purchase.find({"Date":querry}):
+        amount +=(i['Total'])
+        arr.append({'Productname':i['Productname'],
+                    'Variety':i['Variety'],
+                    'Brand':i['Brand'],
+                    "total Amount":i["Total"],
+                    'Quantity':i['Quantity'],
+                    "Company":i["Company"]}
+                    )
+    return (arr, amount)
+
+def getTodaysSaleRecords(querry): #Returns total amount of sale and total records of sale of present day
+    arr = []
+    amount = 0
+    for i in sales.find({"Date":querry}):
+        amount +=(i['Total'])
+        print(i['Date'])
+        arr.append({'Productname':i['Productname'],
+                    'Variety':i['Variety'],
+                    'Brand':i['Brand'],
+                    "total":i["Total"],
+                    'Quantity':i['Quantity'],
+                    "Company":i["Company"]})
+        
+    return (arr, amount)
+
+def getOutOfStock(qt):
+    querry = {'$lte':qt}
+    return inventory.find({"Quantity": querry})
+
+class sform(FlaskForm): #Sales form class
+    
+    product = SelectField('Select Crop',[validators.DataRequired("Please Select Crop.")] ,choices = [])
+    variety = SelectField('Select Variety',[validators.DataRequired("Please Select Variety.")], choices = [])
+    Brand = SelectField('Select Brand',[validators.DataRequired("Please Select Brand.")], choices = [])     
+     
+    name = TextField("Customer Name ",[validators.DataRequired("Please enter Name.")])
+    city = TextField("Customer City ",[validators.DataRequired("Please enter City.")])  
+    c_comp = TextField("Company",[validators.DataRequired("Please enter customer's Brand name.")])  
+    price  = IntegerField("Selling Price",[validators.DataRequired("Please enter selling price.")])
+    quan = IntegerField("Quantity",[validators.DataRequired("Please enter Quantity in Kilograms.")])  
+    warehouse = IntegerField("Warehouse #",[validators.DataRequired("Please enter Warehouse #.")])
+
+    transaction = SelectField('Select Debit/Credit',[validators.DataRequired("Please Select Debit/Credit.")], choices = [('Debit','Debit'),('Credit','Credit')])
+  
+    submit = SubmitField("Submit")  
+
+class addinform(FlaskForm): #Form class for adding Quantity in existing item
+    product = SelectField('Select Crop',[validators.DataRequired("Please Select Crop.")] ,choices = get_productname())
+    variety = SelectField('Select Variety',[validators.DataRequired("Please Select Variety.")], choices = [])
+    Brand = SelectField('Select Brand',[validators.DataRequired("Please Select Brand.")], choices = [])     
+    price  = IntegerField("Purchase price per KG",[validators.DataRequired("Please enter price.")])
+    quan = IntegerField("Quantity",[validators.DataRequired("Please enter Quantity in Kilograms.")])  
+
+    name = TextField("Name ",[validators.DataRequired("Please enter Name.")])
+    city = TextField("City ",[validators.DataRequired("Please enter City.")])  
+    c_comp = TextField("Company",[validators.DataRequired("Please enter customer's Brand name.")])  
+
+    warehouse = IntegerField("Warehouse #",[validators.DataRequired("Please enter Warehouse.")])  
+       
+    transaction = SelectField('Select Debit/Credit',[validators.DataRequired("Please Select Debit/Credit.")], choices = [('Debit','Debit'),('Credit','Credit')])
+
+
+    submit = SubmitField("Submit")  
+
+class addnewform(FlaskForm): #Form class for adding a new item in inventory
+
+    product = TextField('Crop Name',[validators.DataRequired("Please insert Crop.")])
+    variety = TextField('Variety',[validators.DataRequired("Please insert Variety.")])
+    Brand = TextField('Brand',[validators.DataRequired("Please insert Brand.")])     
+    price  = IntegerField("Purchase price per KG",[validators.DataRequired("Please enter price.")])
+    quan = IntegerField("Quantity",[validators.DataRequired("Please enter Quantity.")])
+    warehouse = IntegerField("Warehouse #",[validators.DataRequired("Please enter Warehouse.")])  
+   
+    name = TextField("Name ",[validators.DataRequired("Please enter Name.")])
+    city = TextField("City ",[validators.DataRequired("Please enter City.")])  
+    c_comp = TextField("Company",[validators.DataRequired("Please enter customer's Brand name.")])  
+
+    transaction = SelectField('Select Debit/Credit',[validators.DataRequired("Please Select Debit/Credit.")], choices = [('Debit','Debit'),('Credit','Credit')])
+
+    submit2 = SubmitField("Submit")
+
+class searchform(FlaskForm): #Form class for adding a new item in inventory
+    product = TextField('Crop Name')
+    variety = TextField('Variety')
+    Brand = TextField('Brand')      
+    name = TextField("Buyer's Name ")
+    city = TextField("City")  
+    c_comp = TextField("Company")  
+
+    transaction = SelectField('Select Debit/Credit', choices = [('','Both'),('Debit','Debit'),('Credit','Credit')])
+
+    submit2 = SubmitField("Submit")
+
+
+def getQuantityFromWh():
+    arr = []
+    s = inventory.find({"Warehouse": 1})
+    c=0
+    for i in s:
+        c+=i['Quantity']
+    arr.append(c)
+
+    s = inventory.find({"Warehouse": 2})
+    c=0
+    for i in s:
+        c+=i['Quantity']
+    arr.append(c)
+
+    s = inventory.find({"Warehouse": 3})
+    c=0
+    for i in s:
+        c+=i['Quantity']
+    arr.append(c)
+    return arr
+
+def foo():
+    def getLast6Month(days):
+        return(datetime.now() - timedelta(days=days))
+    
+    days = 0
+
+    for i in range(6):
+        days += 30
+        yield(getLast6Month(days))
+
+def getSalesOf6Months():
+    arr = []  
+    tot = []
+    for i in foo():
+        querry = {'$lt':i+timedelta(30), '$gt': i}
+        cursor = sales.find({"Date":querry })
+        total = 0
+        for i in cursor:
+            total+=i['Total']
+        arr.append(querry['$gt'].strftime('%b-%y') + ' Sales :' )
+        tot.append(total)
+    return (arr, tot)
+
